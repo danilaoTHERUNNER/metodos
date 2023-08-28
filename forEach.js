2ffb1eb931bcb5965b1af4c3e20f2eb3b75635a7
danilaoTHERUNNER <142600024+danilaoTHERUNNER@users.noreply.github.com>
2023-08-28 11:36:45 -0400

    Add files via upload

---
+++ forEach.js
@@ -0,0 +1,6 @@
+forEach()
+
+const arr = [2,4,6,8];
+arr.forEach(num => {
+  console.log(num); saída: 2 4 6 8
+});
---
+++ map.js
@@ -0,0 +1,7 @@
+map()
+
+const arr = [2,4,6,8];
+const newArr = arr.map(num => num * 2);
+console.log(newArr) // saída: [4,8,12,16]
+
+
